c0740d588197ee0b2eb8d3ada4a86e92d7135146
Jacopo <49444372+jacopodona@users.noreply.github.com>
2022-12-22 12:27:09 +0100

    added scripts for cifar10c

---
+++ autoNovel/scripts/download_cifar10_c_weights.sh
@@ -0,0 +1,18 @@
+cd data
+mkdir -p experiments
+cd experiments
+mkdir -p supervised_learning
+mkdir -p auto_novel
+mkdir -p selfsupervised_learning
+
+cd selfsupervised_learning
+gdown https://drive.google.com/uc?id=1bArug5cAvLyYBYBrVzPYZWyGegN2pJ1U
+cd ..
+
+cd supervised_learning
+gdown https://drive.google.com/uc?id=1BLgbDUfcRwoU4iLDyTX3UQNw1Vj2pX73
+cd ..
+
+cd auto_novel
+gdown https://drive.google.com/uc?id=1gmHq70IzVkDsLdN7jFT4cKokZInSgdzg
+cd ..
